57e26f232be71c72c102bfe8c1e366fc1192e4d8
jack-michaud <mrmichaud42@gmail.com>
2017-08-08 20:32:04 -0400

    start webserver (lol)

---
+++ site/server.py
@@ -0,0 +1,23 @@
+import csv
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    context = {"test": "Jack"}
+    return render_template('index.html', **context)
+
+@app.route("/data")
+def fetch_data():
+    csvfile = open('logs.csv', 'rb')
+    reader = csv.reader(csvfile, delimiter=',')
+    context = []
+    for row in reader:
+        context.append({"date": row[0], "hostname": row[1], "ip": row[2], "mac": row[3]})
+
+    csvfile.close()
+    return jsonify(context)
+
+
+
